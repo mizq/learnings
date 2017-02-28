cecfd27a50ff517f98957654023933ce4f521210
mizq <mantizquierdo@gmail.com>
2017-02-28 18:33:04 +0100

    commit MAV vs Simple

---
+++ backendninja/src/main/java/com/udemy/controller/ExampleController.java
@@ -0,0 +1,29 @@
+package com.udemy.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@RequestMapping( "/example" ) 
+public class ExampleController {
+
+	public static final String EXAMPLE_VIEW = "example";
+	
+	//Primera forma. Redirecciones o insertar pocos datos
+	@GetMapping( "/exampleString")
+	// Hace lo mismo que: @RequestMapping( value="/exampleString", method=RequestMethod.GET )
+	public String exampleString()
+	{
+		return EXAMPLE_VIEW;
+	}
+	
+	//Segunda forma. Mejor para insertar datos en plantillas
+	@GetMapping( "/exampleMAV")
+	public ModelAndView exampleMAV()
+	{
+		return new ModelAndView( EXAMPLE_VIEW );
+	}
+	
+}
