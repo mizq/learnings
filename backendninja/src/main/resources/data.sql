42260f7b7287d4ab93dc94d0fba592223c3b452f
mizq <mantizquierdo@gmail.com>
2017-03-15 19:00:51 +0100

    H2, JPA Repository ( new data )

--- backendninja/src/main/resources/data.sql
+++ backendninja/src/main/resources/data.sql
@@ -1,2 +1,4 @@
-INSERT INTO course ( name, description, price, hours ) VALUES ( 'J2EE', 'J2EE course', '200', '2' );
-INSERT INTO course ( name, description, price, hours ) VALUES ( 'Angular', 'Angular course', '300', '4' );
+INSERT INTO course ( name, description, price, hours ) VALUES ( 'J2EE', 'J2EE Course', '200', '2' );
+INSERT INTO course ( name, description, price, hours ) VALUES ( 'Angular', 'Angular Course', '300', '4' );
+INSERT INTO course ( name, description, price, hours ) VALUES ( 'jQuery', 'jQuery Course', '34', '12' );
+INSERT INTO course ( name, description, price, hours ) VALUES ( 'HTML', 'HTML Course', '56', '9' );
