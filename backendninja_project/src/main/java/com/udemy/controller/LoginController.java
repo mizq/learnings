2dfc47e21df266d1279f7d7f08313d346d35cfa8
mizq <mantizquierdo@gmail.com>
2017-03-26 17:50:24 +0200

    pull

--- backendninja_project/src/main/java/com/udemy/controller/LoginController.java
+++ backendninja_project/src/main/java/com/udemy/controller/LoginController.java
@@ -9,12 +9,25 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 import com.udemy.constant.ViewConstant;
 
+// TODO: Auto-generated Javadoc
+/**
+ * The Class LoginController.
+ */
 @Controller
 public class LoginController {
 
+	/** The Constant LOG. */
 	private static final Log LOG = LogFactory.getLog(LoginController.class);
 
 	
+	/**
+	 * Show login form.
+	 *
+	 * @param model the model
+	 * @param logout the logout
+	 * @param error the error
+	 * @return the string
+	 */
 	@GetMapping("/login")
 	public String showLoginForm(Model model, @RequestParam(name = "logout", required = false) String logout,
 			@RequestParam(name = "error", required = false) String error) {
@@ -29,6 +42,11 @@ public class LoginController {
 		return ViewConstant.LOGIN;
 	}
 
+	/**
+	 * Login check.
+	 *
+	 * @return the string
+	 */
 	@GetMapping( { "/", "/loginSuccess" } )
 	public String loginCheck( ) {
 
