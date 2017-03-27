8de9a78139886c8e8960d0d3c2fdf1713db7edcb
mizq <mantizquierdo@gmail.com>
2017-03-27 11:18:41 +0200

    RestController

---
+++ backendninja_project/src/main/java/com/udemy/controller/BackendninjaRestController.java
@@ -0,0 +1,23 @@
+package com.udemy.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.udemy.model.ContactModel;
+
+@RestController
+@RequestMapping( "/rest" )
+public class BackendninjaRestController {
+
+	@GetMapping( "/checkrest" )
+	public ResponseEntity<ContactModel> checkRest(){
+		
+		ContactModel contactModel = new ContactModel( Long.getLong("2"), "Mikel", "Perez", "987654321", "Madrid" ); 
+		return new ResponseEntity<ContactModel>( contactModel , HttpStatus.OK );
+	}
+	
+	
+}
